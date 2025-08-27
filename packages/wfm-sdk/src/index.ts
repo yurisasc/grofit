0ea3a801dcc12eba3cd8914444af28a4a350643e
yurisasc <yuris_aryansyah@yahoo.com>
2025-08-28 02:06:02 +1000

    refactor(wfm-sdk): type exports

--- packages/wfm-sdk/src/index.ts
+++ packages/wfm-sdk/src/index.ts
@@ -1,6 +1,2 @@
 export { WarframeMarketAPI } from './client'
-
-// Export all types for consumers
-export * from './types/common'
-export * from './types/items'
-export * from './types/orders'
+export * from './types'
